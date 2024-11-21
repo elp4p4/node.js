873f1a3c133a097914343bd173c85c147f6d940d
Ahmed <brazfu145@gmail.com>
2024-11-21 18:23:15 +0100

    modify

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((_req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello Node!!!!</h1>\n');
 });
