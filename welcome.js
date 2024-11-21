6d6b2a79de2304f0fc7b8e2c82170a6cb08f3d82
Ahmed <brazfu145@gmail.com>
2024-11-21 18:21:31 +0100

    modify

--- welcome.js
+++ welcome.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
-fs.readFile('welcome.txt', 'utf8', (err, data) => {
+fs.writeFile('welcome.txt', 'Hello Node', (err) => {
     if (err) {
-        console.error('Error reading welcome.txt:', err.message);
+        console.error('Error writing to file:', err);
         return;
     }
-    console.log(data);
+    console.log('File "welcome.txt" created successfully!');
+
+    fs.readFile('welcome.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return;
+        }
+        console.log(data);
+    });
 });
